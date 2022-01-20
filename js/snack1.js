e2b3178660ee38d03e6dd01d7381d3e18ef60b5c
AdrianaSantorio <adriana.santorio@gmail.com>
2022-01-20 14:42:42 +0100

    initialized repository

---
+++ js/snack1.js
@@ -0,0 +1,8 @@
+/*Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare in console la bici con peso minore utilizzando destructuring e template literal
+*/
+
+
+
