d335c489a5949d4fd705808080cb136b6dec5182
AdrianaSantorio <adriana.santorio@gmail.com>
2022-01-20 14:51:36 +0100

    obtained weigth variable

--- js/snack1.js
+++ js/snack1.js
@@ -4,5 +4,28 @@ Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e pe
 Stampare in console la bici con peso minore utilizzando destructuring e template literal
 */
 
+/* 
+1- creare un array di oggetti
+2- ricavare la variabile peso
+3- trovare il peso minore
+*/
 
+
+const bicycles = [
+    {name: 'Road bike', weight: 358,},
+    {name: 'Cruise', weight: 372,},
+    {name: 'Fixed Gear', weight: 451,},
+    {name: 'Mountain Bike', weight: 763,},
+    {name: 'BMX', weight: 358,},
+    {name: 'Touring Bike', weight: 298,},
+    {name: 'Recumbent Bike', weight: 287,},
+    {name: 'Folding Bike', weight: 957,},
+    {name: 'Utility Bike', weight: 530,},
+];
+
+for (let i = 0; i < bicycles.length; i++) { //ciclo relativo all' array
+    const {weight} = bicycles[i]; //2
+    console.log(weight);
+
+};
 
