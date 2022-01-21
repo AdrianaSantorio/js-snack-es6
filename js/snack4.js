815ff1bd55e75fcebc4d50844c6749fd26cf95e0
AdrianaSantorio <adriana.santorio@gmail.com>
2022-01-21 18:26:47 +0100

    added students name to plaques

--- js/snack4.js
+++ js/snack4.js
@@ -6,5 +6,27 @@ ES (marco de iulio => Marco de iulio);
 2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
 BONUS  nei nomi sulle targhe l'iniziale di ogni parola deve essere maiuscola
-
 */
+
+const students = [
+    {id:344, name:'marco Rossi', grades:45,},
+    {id:123, name:'pietro Bianchi', grades:87,},
+    {id:124, name:'giuseppe Verdi', grades:47,},
+    {id:357, name:'luca Pervinca', grades:48,},
+    {id:245, name:'azzurra Celeste', grades:81,},
+    {id:089, name:'violetta Mogano', grades:54,},
+    {id:111, name:'gemma Oro', grades:56,},
+    {id:101, name:'claudia Neri', grades:09,},
+    {id:001, name:'lucia Rosa', grades:54,},
+    {id:002, name:'rosa de Rossi', grades:23,},
+    {id:345, name:'carlo cremisi', grades:65,},
+    {id:045, name:'martina smeraldo', grades:13,},
+    {id:067, name:'francesco cielo', grades:54,},
+    {id:145, name:'lorenzo de klein', grades:54,},
+];
+
+const plaques = students.map ((item) => {
+    return item.name;
+});
+
+console.log(plaques);
